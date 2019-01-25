b164767658f11c8b9cfef857c203e13bc6251601
Vincent <vincentiu.adam@gmail.com>
2019-01-26 00:48:46 +0100

    Added simple sprite renderer ray caster
    Added a Stick class to define interactable sticks
    
    Implemented simple hover mechanic
    
    Stick changes color on hover

---
+++ Assets/Sources/Raycaster.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Raycaster : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask m_RaycastLayer;
+
+    private Camera m_Camera;
+
+    private Stick m_OldSelection;
+
+    private void Awake()
+    {
+        m_Camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        Stick selection = GetHitOnMousePosition();
+        if (selection != m_OldSelection)
+        {
+            if (m_OldSelection != null)
+            {
+                m_OldSelection.OnMouseOut();
+            }
+
+            if (selection != null)
+            {
+                selection.OnMouseIn();
+            }
+        }
+
+        m_OldSelection = selection;
+    }
+
+    private Stick GetHitOnMousePosition()
+    {
+        Vector2 mousePosition   = Input.mousePosition;
+        Ray     ray             = m_Camera.ScreenPointToRay(mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, m_Camera.farClipPlane, m_RaycastLayer))
+        {
+            return hit.collider.GetComponent<Stick>();
+        }
+
+        return null;
+    }
+}
---
+++ Assets/Sources/Stick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Stick : MonoBehaviour
+{
+    [SerializeField]
+    private Color m_HoverColor;
+
+    private SpriteRenderer m_SpriteRenderer;
+
+    private Color m_OriginalColor;
+
+    private void Awake()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        m_OriginalColor = m_SpriteRenderer.color;
+    }
+
+    public void OnMouseIn()
+    {
+        m_SpriteRenderer.color = m_HoverColor;
+    }
+
+    public void OnMouseOut()
+    {
+        m_SpriteRenderer.color = m_OriginalColor;
+    }
+}
