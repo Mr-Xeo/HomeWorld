c8224d2a712aa26586ab22dcf61afd3204ff92f9
Vincent <vincentiu.adam@gmail.com>
2019-01-27 13:29:40 +0100

    Added enable object script

---
+++ Assets/Sources/EnableObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class EnableObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_ObjectToToggle;
+
+    public void Toggle()
+    {
+        m_ObjectToToggle.SetActive(!m_ObjectToToggle.activeSelf);
+    }
+}
